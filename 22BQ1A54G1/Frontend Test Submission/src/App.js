6b7e8769f1b8ceb42780e49341c329e8eee308b9
monikavasamsetti <164895211+monikavasamsetti@users.noreply.github.com>
2025-08-01 12:19:28 +0530

    Update App.js

--- 22BQ1A54G1/Frontend Test Submission/src/App.js
+++ 22BQ1A54G1/Frontend Test Submission/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Button, TextField, Typography, Box } from '@mui/material';
 import { Log } from '../../LoggingMiddleware/loggingMiddleware';
 
-const YOUR_TOKEN = "your_token_here"; // Replace with actual token
+const YOUR_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyMmJxMWE1NGcxQHZ2aXQubmV0IiwiZXhwIjoxNzU0MDI5MDM2LCJpYXQiOjE3NTQwMjgxMzYsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiJhOTQxMmZlMy1lZGNjLTQ3ODEtODliNi1hNTM2ZDAyMGE3OTIiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJ2YXNhbXNldHRpIG1vbmlrYSBkdXJnYSBzYXR5YSB2YW5pIiwic3ViIjoiMTU2MTlmMDMtM2E3ZS00MzFkLTlkNzItMTYwZDNiMmYyNzFmIn0sImVtYWlsIjoiMjJicTFhNTRnMUB2dml0Lm5ldCIsIm5hbWUiOiJ2YXNhbXNldHRpIG1vbmlrYSBkdXJnYSBzYXR5YSB2YW5pIiwicm9sbE5vIjoiMjJicTFhNTRnMSIsImFjY2Vzc0NvZGUiOiJQblZCRlYiLCJjbGllbnRJRCI6IjE1NjE5ZjAzLTNhN2UtNDMxZC05ZDcyLTE2MGQzYjJmMjcxZiIsImNsaWVudFNlY3JldCI6Im5CUlh0Sm1yeEFmVnpQZFIifQ.KD4h2LNB25B4FxXXTmDfvc3pdz2NE5P02-aTBsOSlis"; // Replace with actual token
 
 function App() {
   const [url, setUrl] = useState("");
